320260ff12770518d8034de20b4572bbed950ec5
salman-accuknox <86464007+salman-accuknox@users.noreply.github.com>
2021-12-07 20:42:28 +0530

    Create 2-create-entries.sh

---
+++ cilium-spire/2-create-entries.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+# 2-create-entries.sh
+# cilium-agent
+# This entry is needed to be sure that the cilium agent is able to use the spire
+# privileged API. The unix:uid:0 selector is used because cilium-agent runs as a
+# process in the host in the dev environment. If cilium-agent is run as a pod
+# then the k8s selectors for that pod should be used.
+kubectl exec -n spire spire-server-0 -- \
+    /opt/spire/bin/spire-server entry create \
+    -spiffeID spiffe://example.org/ciliumagent \
+    -parentID spiffe://example.org/ns/spire/sa/spire-agent \
+    -selector unix:uid:0
+
+# agent
+kubectl exec -n spire spire-server-0 -- \
+    /opt/spire/bin/spire-server entry create \
+    -node  \
+    -spiffeID spiffe://example.org/ns/spire/sa/spire-agent \
+    -selector k8s_sat:cluster:demo-cluster \
+    -selector k8s_sat:agent_ns:spire \
+    -selector k8s_sat:agent_sa:spire-agent
+
+kubectl exec -n spire spire-server-0 -- \
+    /opt/spire/bin/spire-server entry create \
+    -spiffeID spiffe://example.org/frontend \
+    -parentID spiffe://example.org/ns/spire/sa/spire-agent \
+    -selector k8s:pod-label:app:frontend 
+
+kubectl exec -n spire spire-server-0 -- \
+    /opt/spire/bin/spire-server entry create \
+    -spiffeID spiffe://example.org/cartservice \
+    -parentID spiffe://example.org/ns/spire/sa/spire-agent \
+    -selector k8s:pod-label:app:cartservice
