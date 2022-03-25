847c6ad38044b9cb922ae673fde86e632f653c03
salman-accuknox <86464007+salman-accuknox@users.noreply.github.com>
2022-03-25 09:16:28 +0530

    Update deploy-app.sh

--- MongoDB/deploy-app.sh
+++ MongoDB/deploy-app.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-kubectl create -f tasksapp.yaml
+kubectl create -f https://raw.githubusercontent.com/accuknox/samples/main/MongoDB/tasksapp.yaml
 #kubectl scale deployment tasksapp --replicas=3
-kubectl create -f tasksapp-svc.yaml
-kubectl create -f mongo-pv.yaml
-kubectl create -f mongo-pvc.yaml
-kubectl create -f mongo.yaml
-kubectl create -f mongo-svc.yaml
+kubectl create -f https://raw.githubusercontent.com/accuknox/samples/main/MongoDB/tasksapp-svc.yaml
+kubectl create -f https://raw.githubusercontent.com/accuknox/samples/main/MongoDB/mongo-pv.yaml
+kubectl create -f https://raw.githubusercontent.com/accuknox/samples/main/MongoDB/mongo-pvc.yaml
+kubectl create -f https://raw.githubusercontent.com/accuknox/samples/main/MongoDB/mongo.yaml
+kubectl create -f https://raw.githubusercontent.com/accuknox/samples/main/MongoDB/mongo-svc.yaml
